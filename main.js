0d4fb2aedd23a8dbf0f732e1f0a1dd714f1a6f57
Роман <senior.starkoff2018@yandex.ru>
2024-01-17 22:30:43 +0300

    Done refactoring

--- main.js
+++ main.js
@@ -16,7 +16,19 @@ let won = false
 
 let field = creatingField()
 
-function creatingField(){
+function reset() {
+    console.log(field)
+    field = creatingField();
+    for (let i = 0; i < box.length; i++) {
+        const element = box[i];
+        element.style.backgroundImage = '';
+    }
+    cross.style.visibility = 'hidden'
+    nought.style.visibility = 'hidden'
+    won = false
+}
+
+function creatingField() {
     let techField = []
     let len = 0
     for (i = 0; i < 3; i++) {
@@ -29,16 +41,7 @@ function creatingField(){
     return techField;
 }
 
-resetingButton.addEventListener('click', () => {
-    field = creatingField();
-    for (let i = 0; i < box.length; i++) {
-        const element = box[i];
-        element.style.backgroundImage = '';
-    }
-    cross.style.visibility = 'hidden'
-    nought.style.visibility = 'hidden'
-    won = false
-})
+resetingButton.addEventListener('click', reset)
 
 board.addEventListener('click', (event) => {
     const selectedItem = event.target;
@@ -46,119 +49,125 @@ board.addEventListener('click', (event) => {
 })
 
 function makeTurn(selectedItem) {
-    for (let y = 0; y < field.length; y++) {
-        const x = field[y].map((item, index) => {
-            if (item === selectedItem.id) {
+    if (won === true) {
+        return
+    }
+    for (let x = 0; x < field.length; x++) {
+        for (let y = 0; y < field[0].length; y++) {
+            if (field[x][y] === selectedItem.id) {
                 if (isCross === true) {
-                    field[y][index] = 'x';
+                    field[x][y] = 1;
                     selectedItem.style.backgroundImage = 'url(images/cross.png)';
                     isCross = false;
                 }
                 else {
                     selectedItem.style.backgroundImage = 'url(images/nought.png)';
-                    field[y][index] = 'o';
+                    field[x][y] = -1;
                     isCross = true;
                 }
             }
-        })
+        }
     }
 }
 
+function isAnyFreeCell() {
+    let counter = 0
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j] === -1 || field[i][j] === 1) {
+                counter += 1
+            }
+        }
+    }
+    if (counter === 9) {
+        return false
+    }
+    return true
+}
+
 function changes(selectedItem) {
-    makeTurn(selectedItem);   
+    makeTurn(selectedItem);
     endGameLogic();
 }
 
-function endGameLogic () {
-    isWonRow();
-    isWonCoulumn();
-    isWonDiaganal();
-    isWonDiaganalSecond();
+function endGameLogic() {
+    if (isAnyFreeCell()) {
+        const won_row = isWonRow();
+        const won_col = isWonCoulumn();
+        const main_diagon_won = isWonDiaganal();
+        const addied_diag_won = isWonDiaganalSecond();
+    }
+    if(won === false && !isAnyFreeCell()) {
+        // need create visual for tie
+        console.log(1)
+    }
 }
 
 function isWonRow() {
     let countWin = 0;
-    for(let i = 0; i < field.length; i++) {
-        for(let j = 0; j < field[i].length; j++){
-            if (field[i][j] === 'x'){
-                countWin += 1;
-            }
-            if (field[i][j] === 'o'){
-                countWin -= 1;
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j] === -1 || field[i][j] === 1) {
+                countWin += field[i][j];
             }
         }
-        if (countWin === 3) {
-            alertWin(3)
-            break
+        if (countWin === 3 || countWin === -3) {
+            alertWin(countWin)
+            return i
         }
-        if (countWin === -3) {
-            alertWin(-3)
-            break
-        }
-        else{
-            countWin = 0;
-        }
+        countWin = 0
     }
 }
 
 function isWonCoulumn() {
     let countWin = 0;
-    for(let i = 0; i < field.length; i++) {
-        for(let j = 0; j < field[i].length; j++){
-            if (field[j][i] === 'x'){
-                countWin += 1;
-            }
-            if (field[j][i] === 'o'){
-                countWin -= 1;
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[j][i] === -1 || field[j][i] === 1) {
+                countWin += field[j][i];
             }
         }
-        if (countWin === 3) {
-            alertWin(3)
-            break
+        if (countWin === 3 || countWin === -3) {
+            alertWin(countWin)
+            return i
         }
-        if (countWin === -3) {
-            alertWin(-3)
-            break
-        }
-        else{
-            countWin = 0;
-        }
+        countWin = 0
     }
 }
 
-function isWonDiaganal(){
+function isWonDiaganal() {
     let countWin = 0;
-    for (let i = 0; i < field.length; i++){
-        if (field[i][i] === 'x'){
-            countWin += 1;
-        }
-        if (field[i][i] === 'o'){
-            countWin -= 1;
+    for (let i = 0; i < field.length; i++) {
+        if (field[i][i] === -1 || field[i][i] === 1) {
+            countWin += field[i][i];
         }
     }
-    alertWin(countWin)
+    if (countWin === 3 || countWin === -3) {
+        alertWin(countWin)
+        return countWin
+    }
 }
 
-function isWonDiaganalSecond(){
+function isWonDiaganalSecond() {
     let countWin = 0;
-    for (let i = 0; i < field.length; i++){
-        if (field[i][2 - i] === 'x'){
-            countWin += 1;
-        }
-        if (field[i][2 - i] === 'o'){
-            countWin -= 1;
+    for (let i = 0; i < field.length; i++) {
+        if (field[i][2 - i] === -1 || field[i][2 - i] === 1) {
+            countWin += field[i][2 - i];
         }
     }
-    alertWin(countWin)
+    if (countWin === 3 || countWin === -3) {
+        alertWin(countWin)
+        return countWin
+    }
 }
 
-function alertWin(countWin){
-    if (countWin === 3 && won !== true) {
+function alertWin(countWin) {
+    if (countWin === 3 && won === false) {
         cross.style.visibility = 'visible'
         nought.style.visibility = 'hidden'
         won = true
     }
-    if (countWin === -3 && won !== true) {
+    if (countWin === -3 && won === false) {
         nought.style.visibility = 'visible'
         cross.style.visibility = 'hidden'
         won = true
